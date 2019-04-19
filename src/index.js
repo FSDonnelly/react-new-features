2252882efdbba095fe4e1a82af3593b2afe888f4
FSDonnelly <donnelly_frank@yahoo.com>
2019-04-18 22:30:56 -0700

    example of useSate hook in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<div>Mu New Content</div>, document.getElementById('root'));
+// { count: 0, name: '' }
+
+const App = () => {
+    const [count, setCount] = useState(15)
+
+    const increment = () =>{
+        setCount(count + 1)
+    }
+
+    return (
+        <div>
+            <p>The Current Count is {count}</p>
+            <button onClick={increment}>+1</button>
+        </div>
+    )
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

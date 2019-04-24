dda09002df27cfe100e580496a3a45bf50029641
FSDonnelly <donnelly_frank@yahoo.com>
2019-04-23 23:03:21 -0700

    example of useContext

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import NotesContext from '../context/notes-context'
 
-const Note = ({ note, removeNote }) => {
+const Note = ({ note }) => {
+    const { dispatch } = useContext(NotesContext)
+
     return (
         <div>
             <li>{note.title}</li>
             <p>{note.body}</p>
-            <button onClick={() => removeNote(note.title)}>x</button>
+            <button onClick={() => dispatch({ type: 'REMOVE_NOTE', title: note.title })}>x</button>
         </div>
     )
 }
